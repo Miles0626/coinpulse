7c44448709b16bb75fff03e061b450429f50095b
caibo <shelby-family@outlook.com>
2026-09-25 10:38:51 +0800

    feat: add header navigation

---
+++ components/Header.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { cn } from 'cn';
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation';
+import React from 'react'
+
+const Header = () => {
+    const pathName = usePathname();
+
+    return (
+        <header>
+            <div className='main-container inner'>
+                <Link href='/'>
+                    <Image src='/logo.svg' alt='CoinPulse logo' width={132} height={40}></Image>
+                </Link>
+                <nav>
+                    <Link href='/' className={cn('nav-link', {
+                        'is-active': pathName === '/',
+                        'is-home': true
+                    })}>Home</Link>
+                    <p>Search Modal</p>
+                    <Link href='/coins' className={cn('nav-link', {
+                        'is-active': pathName === '/coins'
+                    })}>All Coins</Link>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default Header
